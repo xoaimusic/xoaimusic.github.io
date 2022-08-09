05aec2691932620552a8299738f649f4adccb9e2
xoaimusic <110918037+xoaimusic@users.noreply.github.com>
2022-08-09 21:31:10 +0700

    upload

---
+++ js/music-list.js
@@ -0,0 +1,104 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Anh Đánh Rơi Người Yêu Này",
+    artist: "Andiez ft AMEE",
+    img: "Anh Đánh Rơi Người Yêu Này  Andiez ft AMEE",
+    src: "Anh Đánh Rơi Người Yêu Này  Andiez ft AMEE"
+  },
+  {
+    name: "Chỉ Muốn Bên Em Thật Gần (Lofi Ver)",
+    artist: "Y Ling x Nguyễn Minh Phúc",
+    img: "Chỉ Muốn Bên Em Thật Gần Lofi Ver  Y Ling x Nguyễn Minh Phúc",
+    src: "Chỉ Muốn Bên Em Thật Gần Lofi Ver  Y Ling x Nguyễn Minh Phúc"
+  },
+  {
+    name: "có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "có hẹn với thanh xuân  MONSTAR",
+    src: "có hẹn với thanh xuân  MONSTAR"
+  },
+  {
+    name: "dù cho mai về sau",
+    artist: "buitruonglinh",
+    img: "dù cho mai về sau  buitruonglinh",
+    src: "dù cho mai về sau  buitruonglinh"
+  },
+  {
+    name: "Em Khác Gì Hoa (Lofi Ver)",
+    artist: "Lil Zpoet",
+    img: "Em Khác Gì Hoa Lofi Ver  Lil Zpoet",
+    src: "Em Khác Gì Hoa Lofi Ver  Lil Zpoet"
+  },
+  {
+    name: "Em Thích (Lofi Ver)",
+    artist: "Sean x Lửa",
+    img: "Em Thích Lofi Ver  Sean x Lửa",
+    src: "Em Thích Lofi Ver  Sean x Lửa"
+  },
+  {
+    name: "Gác lại âu lo",
+    artist: "Da LAB ft Miu Lê",
+    img: "Gác lại âu lo  Da LAB ft Miu Lê",
+    src: "Gác lại âu lo  Da LAB ft Miu Lê"
+  },
+  {
+    name: "Hai Mươi Hai",
+    artist: "AMEE x Hứa Kim Tuyền",
+    img: "Hai Mươi Hai 22  AMEE x Hứa Kim Tuyền",
+    src: "Hai Mươi Hai 22  AMEE x Hứa Kim Tuyền"
+  },
+  {
+    name: "Lạc Vào Trong Mơ",
+    artist: "Simon C X Wuy",
+    img: "Lạc Vào Trong Mơ  Simon C X Wuy",
+    src: "Lạc Vào Trong Mơ  Simon C X Wuy"
+  },
+  {
+    name: "Lần Hẹn Hò Đầu Tiên (Lofi Ver)",
+    artist: "Huyền Tâm Môn",
+    img: "Lần Hẹn Hò Đầu Tiên Lofi  Huyền Tâm Môn",
+    src: "Lần Hẹn Hò Đầu Tiên Lofi  Huyền Tâm Môn"
+  },
+  {
+    name: "Ngày Đầu Tiên (Lofi Ver)",
+    artist: "Đức Phúc",
+    img: "Ngày Đầu Tiên Lofi Ver  Đức Phúc",
+    src: "Ngày Đầu Tiên Lofi Ver  Đức Phúc"
+  },
+  {
+    name: "Nụ Cười Em Là Nắng (Lofi Ver)",
+    artist: "Green",
+    img: "Nụ Cười Em Là Nắng Lofi Ver  Green",
+    src: "Nụ Cười Em Là Nắng Lofi Ver  Green"
+  },
+  {
+    name: "Sài Gòn Đau Lòng Quá",
+    artist: "Hứa Kim Tuyền ft Hoàng Duyên",
+    img: "Sài Gòn Đau Lòng Quá  Hứa Kim Tuyền ft Hoàng Duyên",
+    src: "Sài Gòn Đau Lòng Quá  Hứa Kim Tuyền ft Hoàng Duyên"
+  },
+  {
+    name: "Về Bên Anh (Lofi Ver)",
+    artist: "Jack-J97",
+    img: "Về Bên AnhLofi Ver Jack",
+    src: "Về Bên AnhLofi Ver Jack"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
